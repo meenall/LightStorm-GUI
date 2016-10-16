6578c43a2573898e107b81159389c6e136e4763a
meenall <meenalk01@gmail.com>
2016-10-16 06:09:57 -0400

    Initial commit

---
+++ Serial/MyForm.h
@@ -0,0 +1,308 @@
+#pragma once
+
+namespace Serial {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System::IO::Ports;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			findPorts();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::IO::Ports::SerialPort^  serialPort1;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::ComboBox^  comboBox2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::ProgressBar^  progressBar1;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::RichTextBox^  textBox1;
+	protected:
+	private: System::ComponentModel::IContainer^  components;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->serialPort1 = (gcnew System::IO::Ports::SerialPort(this->components));
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->SuspendLayout();
+			// 
+			// serialPort1
+			// 
+			this->serialPort1->ReadTimeout = 100;
+			this->serialPort1->WriteTimeout = 500;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->AllowDrop = true;
+			this->comboBox1->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(453, 15);
+			this->comboBox1->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(136, 28);
+			this->comboBox1->TabIndex = 0;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::comboBox1_SelectedIndexChanged);
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->AllowDrop = true;
+			this->comboBox2->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"9600", L"115200" });
+			this->comboBox2->Location = System::Drawing::Point(453, 65);
+			this->comboBox2->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(136, 28);
+			this->comboBox2->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(364, 18);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(76, 20);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"COM Port";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(362, 69);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(78, 20);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Baud Rate";
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->button1->ForeColor = System::Drawing::SystemColors::Control;
+			this->button1->Location = System::Drawing::Point(453, 115);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(136, 30);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Init Port";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->button2->ForeColor = System::Drawing::SystemColors::Control;
+			this->button2->Location = System::Drawing::Point(453, 163);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(136, 30);
+			this->button2->TabIndex = 5;
+			this->button2->Text = L"Close Port";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->BackColor = System::Drawing::SystemColors::Info;
+			this->textBox2->Location = System::Drawing::Point(12, 104);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(292, 28);
+			this->textBox2->TabIndex = 7;
+			this->textBox2->Text = L"Sent";
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->button3->Location = System::Drawing::Point(12, 57);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(142, 36);
+			this->button3->TabIndex = 8;
+			this->button3->Text = L"SEND";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->button4->Location = System::Drawing::Point(160, 57);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(144, 36);
+			this->button4->TabIndex = 9;
+			this->button4->Text = L"READ";
+			this->button4->UseVisualStyleBackColor = false;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// progressBar1
+			// 
+			this->progressBar1->Location = System::Drawing::Point(104, 163);
+			this->progressBar1->Name = L"progressBar1";
+			this->progressBar1->Size = System::Drawing::Size(200, 23);
+			this->progressBar1->TabIndex = 10;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(12, 163);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(86, 20);
+			this->label3->TabIndex = 11;
+			this->label3->Text = L"Port Status:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(12, 12);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(292, 31);
+			this->textBox1->TabIndex = 12;
+			this->textBox1->Text = L"";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::Window;
+			this->ClientSize = System::Drawing::Size(603, 229);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->progressBar1);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->comboBox1);
+			this->Font = (gcnew System::Drawing::Font(L"Arial Unicode MS", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
+			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->Name = L"MyForm";
+			this->Text = L"Light Storm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: void findPorts(void)
+	{
+		array<Object^>^ objectArray = SerialPort::GetPortNames();
+		this->comboBox1->Items->AddRange(objectArray);
+	}
+
+	private: System::Void comboBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		this->textBox1->Text = String::Empty;
+		if (this->comboBox1->Text == String::Empty || this->comboBox2->Text == String::Empty)
+			this->textBox1->Text = "Please select port settings.";
+		else {
+			try {
+				if (!this->serialPort1->IsOpen) {
+					this->serialPort1->PortName = this->comboBox1->Text;
+					this->serialPort1->BaudRate = Int32::Parse(this->comboBox2->Text);
+					this->textBox2->Text = "Enter message here.";
+					this->serialPort1->Open();
+					this->progressBar1->Value = 100;
+				}
+				else
+					this->textBox2->Text = "Port isn't open.";
+			}
+			catch (UnauthorizedAccessException^) {
+				this->textBox2->Text = "Unauthorized Access";
+			}
+		}
+	}
+
+	private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+		String^ name = this->serialPort1->PortName;
+		String^ message = this->textBox2->Text;
+		if (this->serialPort1->IsOpen) {
+			this->serialPort1->WriteLine(message);
+		}
+		else {
+			this->textBox1->Text = "Port Not Open.";
+		}
+	}
+
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		this->serialPort1->Close();
+		this->progressBar1->Value = 0;
+		this->button4->Enabled = true;
+		this->button1->Enabled = true;
+	}
+
+	private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
+		if (this->serialPort1->IsOpen) {
+			this->textBox1->Text = String::Empty;
+			try {
+				this->textBox1->Text = this->serialPort1->ReadExisting();
+			}
+			catch(TimeoutException^) {
+				this->textBox2->Text = "Timeout Exception";
+			}
+			this->button1->Enabled = false;
+		}
+		else {
+			this->textBox1->Text = "Port not open.";
+		}
+	}
+};
+}
